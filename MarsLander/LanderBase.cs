1e26c8afb89a161f22e946129d6b86a8f0255789
LoganEvans <devnull@localhost>
2013-03-21 15:51:55 -0700

    Renamed some files and shuffled the code layout.

--- MarsLander/LanderBase.cs
+++ MarsLander/LanderBase.cs
@@ -131,26 +131,4 @@ namespace MarsLander {
       Console.WriteLine("Status: " + getStatus());
     }
   }
-
-  class Program {
-    public static void UpdateTriggeredEventHandler_print(object sender, UpdateTriggeredEventArgs args) {
-      Console.WriteLine("Height: " + args.height + " Y-Velocity: " + args.yVelocity +
-                        " Position: " + args.xPosition + " X-Velocity: " + args.xVelocity + " Fuel: " + args.fuel);
-    }
-
-    [STAThread]
-    static void Main(string[] args) {
-      LanderBase lander = new LanderBase();
-      Display display = new Display();
-      lander.UpdateTriggered += UpdateTriggeredEventHandler_print;
-      lander.UpdateTriggered += display.UpdateTriggeredEventHandler_paint;
-
-      Thread displayThread = new Thread((ThreadStart)delegate {
-            Application.EnableVisualStyles();
-            System.Windows.Forms.Application.Run(display);
-        });
-      displayThread.Start();
-      lander.simulate(display : true);
-    }
-  }
 }
---
+++ MarsLander/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarsLander {
+  public class Main {
+    public static void UpdateTriggeredEventHandler_print(object sender, UpdateTriggeredEventArgs args) {
+      Console.WriteLine("Height: " + args.height + " Y-Velocity: " + args.yVelocity +
+                        " Position: " + args.xPosition + " X-Velocity: " + args.xVelocity + " Fuel: " + args.fuel);
+    }
+
+    [STAThread]
+    static void Main(string[] args) {
+      LanderBase lander = new LanderBase();
+      Display display = new Display();
+      lander.UpdateTriggered += UpdateTriggeredEventHandler_print;
+      lander.UpdateTriggered += display.UpdateTriggeredEventHandler_paint;
+
+      Thread displayThread = new Thread((ThreadStart)delegate {
+            Application.EnableVisualStyles();
+            System.Windows.Forms.Application.Run(display);
+        });
+      displayThread.Start();
+      lander.simulate(display : true);
+    }
+  }
+}
